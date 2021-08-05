73646d5036b3337bb25bfb251b6ad8d40eb3b0f1
Ethan <crowe2@wwu.edu>
2021-08-05 14:17:39 -0700

    Unit stats are drawn from text file in /stats/. One generic unit class that defaults if given a bad class.

--- game1Units.py
+++ game1Units.py
@@ -29,49 +29,51 @@ def getMoves(r, c, moves, gs):
                 moves.append(new_move)
     return moves
 
-def attack(attacker, defender, gs):
-    #Fire Emblem style combat. Attacker hits first, then defender retaliates.
-    attacker_dmg = attacker.atk - defender.defense
-    defender_dmg = defender.atk - attacker.defense
-    
-    defender.hpLeft -= attacker_dmg
-    #if defender dies, it does not retaliate
-    if defender.hpLeft <= 0:
-        gs.gameMap.mapAddUnit(None, defender.loc[0], defender.loc[1])
-        print("Defender defeated")
-        return
-    attacker.hpLeft -= defender_dmg
-    if attacker.hpLeft <= 0:
-        gs.gameMap.mapAddUnit(None, attacker.loc[0], defender.loc[1])
+'''
+Assigns a unit's stats based on its type.
+If the unit file cannot be found creates a default unit.
+    unit (Unit) = the unit to assign stats to
+    filename (string) = where the stats are determined
+'''
+def readStats(unit, filename):
+    try:
+        file = open("stats/" + filename + ".txt", 'r')
+        file = file.read().split('\n')
+        unit.name = file[0]
+        unit.maxMoves = int(file[1])
+        moveCosts = file[2].split(',')
+        unit.moveCost['g'] = int(moveCosts[0].strip())
+        unit.moveCost['d'] = int(moveCosts[1].strip())
+        unit.moveCost['m'] = int(moveCosts[2].strip())
+        unit.moveCost['w'] = int(moveCosts[3].strip())
+        unit.maxHP = int(file[3])
+        unit.atk = int(file[4])
+        unit.defense = int(file[5])
+    except:
+        unit.name = "Unit"
+        unit.maxMoves = 1
+        unit.moveCost['g'] = 1
+        unit.moveCost['d'] = 1
+        unit.moveCost['m'] = 1
+        unit.moveCost['w'] = 1
+        unit.maxHP = 1
+        unit.atk = 1
+        unit.defense = 1
 
-class Warrior():
-    def __init__(self, team, loc):
-        self.maxMoves = 4
-        self.moveCost = {'g': 1, 'd': 2, 'm': 3, 'w': -1}
-        self.movesLeft = self.maxMoves
+'''
+A generic unit class. Its stats are drawn from a file in the stats folder.
+    name (String) = the unit's type. Determines what file its stats are drawn from.
+    team (char) = which team the unit is on ('u' or 'e')
+    loc (tuple) = coordinates where the unit will be placed
+'''
+class Unit():
+    def __init__(self, name, team, loc):
+        self.moveCost = {}
         self.team = team
-        self.name = 'W'
-        self.image = self.team + self.name
         self.loc = loc
-        
-        self.didAttack = False
-        self.maxHP = 20
-        self.hpLeft = self.maxHP
-        self.atk = 10
-        self.defense = 5
-        
-class Tarantula():
-    def __init__(self, team, loc):
-        self.maxMoves = 3
-        self.moveCost = {'g': 2, 'd': 1, 'm': 2, 'w': -1}
+        readStats(self, name)
+        self.image = self.team + self.name
         self.movesLeft = self.maxMoves
-        self.team = team
-        self.name = "Tarantula"
-        self.image = self.team + self.name
-        self.loc = loc
-        
-        self.didAttack = False
-        self.maxHP = 25
         self.hpLeft = self.maxHP
-        self.atk = 12
-        self.defense = 8
+        self.didAttack = False
+    
